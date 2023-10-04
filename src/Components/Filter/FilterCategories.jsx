1429d3cad6b8e7d570c9b3d0330510be5f58494c
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-04 13:42:55 +0200

    Add active class to a selected category

--- src/Components/Filter/FilterCategories.jsx
+++ src/Components/Filter/FilterCategories.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import classes from "./FilterCategories.module.css";
 
 const FilterCategories = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = ["Sushi", "Rolls", "Royal", "Sushi sets"];
   return (
     <>
       <ul className={classes.listWrapper}>
-        <li>Sushi</li>
-        <li>Rolls</li>
-        <li>Royal</li>
-        <li>Sushi sets</li>
+        {categories.map((value, i) => (
+          <li
+            key={value}
+            onClick={() => setActiveIndex(i)}
+            className={activeIndex === i ? classes.active : ""}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
-      <h2>lol</h2>
     </>
   );
 };
