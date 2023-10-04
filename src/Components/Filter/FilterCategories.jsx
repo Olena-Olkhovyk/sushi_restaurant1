3442ad7a3cb1313a7a79c653adf12d82200b55f5
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-04 11:21:07 +0200

    Add and style filter list

---
+++ src/Components/Filter/FilterCategories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./FilterCategories.module.css";
+
+const FilterCategories = () => {
+  return (
+    <>
+      <ul className={classes.listWrapper}>
+        <li>Sushi</li>
+        <li>Rolls</li>
+        <li>Royal</li>
+        <li>Sushi sets</li>
+      </ul>
+      <h2>lol</h2>
+    </>
+  );
+};
+export default FilterCategories;
