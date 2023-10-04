bb5eff373845ffbd9f8add4cdb4a33087ce35ff2
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-04 18:53:31 +0200

    Add functionality for opening and closing popup

--- src/Components/Sort/Sort.jsx
+++ src/Components/Sort/Sort.jsx
@@ -3,23 +3,26 @@ import classes from "./Sort.module.css";
 
 const Sort = () => {
   const [selected, setSelected] = React.useState(0);
+  const [open, setOpen] = React.useState(false);
   const sortList = ["popularity", "price", "a-z"];
   return (
     <div className={classes.sort}>
       <b>Sort by: </b>
-      <span>popularity</span>
+      <span onClick={() => setOpen(!open)}>popularity</span>
       <div className={classes.sortPopUp}>
-        <ul className={classes.listWrapper}>
-          {sortList.map((val, i) => (
-            <li
-              key={val}
-              onClick={() => setSelected(i)}
-              className={selected === i ? classes.active : ""}
-            >
-              {val}
-            </li>
-          ))}
-        </ul>
+        {open && (
+          <ul className={classes.listWrapper}>
+            {sortList.map((val, i) => (
+              <li
+                key={val}
+                onClick={() => setSelected(i)}
+                className={selected === i ? classes.active : ""}
+              >
+                {val}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
