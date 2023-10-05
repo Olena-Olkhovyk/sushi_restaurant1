34629f21a0b7134894df5b5f3b827e91acc3bcf9
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-05 20:39:10 +0200

    Get sushi items from mockapi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import SearchInput from "./Components/Searchinput/SearchInput";
 import FilterCategories from "./Components/Filter/FilterCategories";
 import Sort from "./Components/Sort/Sort";
 import Items from "./Components/Items/Items";
+
 function App() {
+  const [item, setItem] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://651ee1a444a3a8aa476925cf.mockapi.io/sushi")
+      .then((res) => res.json())
+      .then((arr) => setItem(arr));
+  }, []);
+
   return (
     <div className="App-wrapper">
       <div className="container">
@@ -19,7 +28,11 @@ function App() {
           <FilterCategories />
         </div>
         <div className="items-container">
-          <Items />
+          <article className="itemCard">
+            {item.map((item) => (
+              <Items key={item.id} {...item} />
+            ))}
+          </article>
         </div>
       </div>
     </div>
