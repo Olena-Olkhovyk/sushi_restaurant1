e15e5b040d11316aa1693149584d9935722e3200
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-10 19:56:06 +0200

    Refactor anchor element for perfomance

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import styles from "./Header.module.css";
 import logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className={styles.Header}>
       <div className={styles.logoImg}>
-        <img src={logo} alt="logo" />
-        <span className={styles.logoName}>Sushi paradise</span>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+          <span className={styles.logoName}>Sushi paradise</span>
+        </Link>
       </div>
       <div className={styles.cartInfo}>
-        <a href="/" className={styles.price}>
+        <Link to="/cart" className={styles.price}>
           0$
-        </a>
-        <a href="/" className={styles.cart}>
+        </Link>
+        <Link to="/cart" className={styles.cart}>
           <i className="fa-solid fa-cart-shopping"></i>0
-        </a>
+        </Link>
       </div>
     </div>
   );
