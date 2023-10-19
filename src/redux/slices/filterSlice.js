c235360e5d7f3e283bd642da63a79c8197f3748f
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-19 15:11:56 +0200

    Add filterSlice to manage filter state

---
+++ src/redux/slices/filterSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  categoryId: 0,
+};
+const filterSlice = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+    setCategoryId(state, action) {
+      state.categoryId = action.payload;
+    },
+  },
+});
+
+export const { setCategoryId } = filterSlice.actions;
+export default filterSlice.reducer;
