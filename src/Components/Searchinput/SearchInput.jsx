599fe00407279bf3fb3314bc086c495c7d915892
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-21 11:29:10 +0200

    Optimize network requests with debounce functionality

--- src/Components/Searchinput/SearchInput.jsx
+++ src/Components/Searchinput/SearchInput.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./SearchInput.css";
 import { SearchContext } from "../../App";
+import debounce from "lodash.debounce";
 
 const SearchInput = () => {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const [value, setValue] = React.useState("");
+  const { setSearchValue } = React.useContext(SearchContext);
+  const updatedSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000),
+    []
+  );
+  const onChangeValue = (e) => {
+    setValue(e.target.value);
+    updatedSearchValue(e.target.value);
+  };
   return (
     <div className="searchField">
       <input
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        value={value}
+        onChange={onChangeValue}
         type="search"
         placeholder="Search sushi"
       />
