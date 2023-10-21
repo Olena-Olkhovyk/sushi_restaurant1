8f3296c91108889710c6f789af1a9bcf45a55e6d
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-21 20:59:55 +0200

    Add redux cart slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filter from "./slices/filterSlice";
+import cart from "./slices/cartSlice";
 //Create storage
 export const store = configureStore({
-  reducer: { filter },
+  reducer: { filter, cart },
 });
