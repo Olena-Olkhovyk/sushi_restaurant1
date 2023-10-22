58e42b99e867490fe5e43dd0ff348230a3a19897
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-22 18:37:51 +0200

    Move cart items to another component

--- src/Components/CartBlock/CartBlock.jsx
+++ src/Components/CartBlock/CartBlock.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./CartBlock.module.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
 
 const CartBlock = () => {
   const { items, totalPrice } = useSelector((state) => state.cart);
@@ -16,24 +17,9 @@ const CartBlock = () => {
         <i className="fa-solid fa-trash"></i>Delete all
       </p>
       <div className={classes.cartContainer}>
-        <div className={classes.itemInfo}>
-          <img
-            src="https://yaposhka.com.ua/media/catalog/product/cache/b8d5666ecec5f6597a39ca6d2a6b3110/b/l/blackdr.webp"
-            className={classes.sushiImg}
-            alt="sushi's item"
-          />
-          <h3>Sushi's name</h3>
-          <p className={classes.sauseInfo}>teriyaki sauce</p>
-        </div>
-        <div className={classes.itemAmount}>
-          <button className={classes.minus}>-</button>
-          <p>2</p>
-          <button className={classes.plus}>+</button>
-        </div>
-        <div className={classes.price}>10$</div>
-        <div className={classes.deleteItem}>
-          <i className="fa-regular fa-circle-xmark"></i>
-        </div>
+        {items.map((item) => (
+          <CartItem key={item.id} {...item} />
+        ))}
       </div>
       <div className={classes.orderDetails}>
         <p>
