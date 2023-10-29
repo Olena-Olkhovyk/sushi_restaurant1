8d95ce194464b100655655c9922819d82157d385
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-29 11:34:25 +0100

    Collapse cart info button when in the cart

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./Header.module.css";
 import logo from "../assets/images/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectCart } from "../../redux/slices/cartSlice";
 const Header = () => {
   const { items, totalPrice } = useSelector(selectCart);
   const totalItems = items.reduce((sum, item) => sum + item.count, 0);
+  const location = useLocation();
   return (
     <div className={styles.Header}>
       <div className={styles.logoImg}>
@@ -15,15 +16,17 @@ const Header = () => {
           <span className={styles.logoName}>Sushi paradise</span>
         </Link>
       </div>
-      <div className={styles.cartInfo}>
-        <Link to="/cart" className={styles.price}>
-          {totalPrice}$
-        </Link>
-        <Link to="/cart" className={styles.cart}>
-          <i className="fa-solid fa-cart-shopping"></i>
-          {totalItems}
-        </Link>
-      </div>
+      {location.pathname != "/cart" && (
+        <div className={styles.cartInfo}>
+          <Link to="/cart" className={styles.price}>
+            {totalPrice}$
+          </Link>
+          <Link to="/cart" className={styles.cart}>
+            <i className="fa-solid fa-cart-shopping"></i>
+            {totalItems}
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
