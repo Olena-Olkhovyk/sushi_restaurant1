3ec562ae3c332e3f116d4e3f816ca3f698512869
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-29 17:43:38 +0100

    Define route for individual product page with dynamic IDs

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import App from "./App";
 import NotFound from "./Components/NotFoundBlock";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import ItemDetail from "./pages/ItemDetail";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -29,6 +31,10 @@ const router = createBrowserRouter([
       {
         path: "*",
         element: <NotFound />,
+      },
+      {
+        path: "/sushi/:id",
+        element: <ItemDetail />,
       },
     ],
   },
