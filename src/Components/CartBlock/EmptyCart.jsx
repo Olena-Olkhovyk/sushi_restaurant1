03ed01d409144debec5bff6f141e1969886d7e31
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-30 22:11:41 +0100

    Add content to EmptyCart component

---
+++ src/Components/CartBlock/EmptyCart.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./CartBlock.module.css";
+import emptyCart from "../assets/images/emptyCart.webp";
+
+const EmptyCart = () => {
+  return (
+    <div className={classes.emptyCartBlock}>
+      <h1>The cart is empty😕</h1>
+      <p>
+        Most likely, you didn't order anything. <br />
+        To order sushi, go back to the main page.
+      </p>
+      <img src={emptyCart} alt="empty cart information" />
+    </div>
+  );
+};
+export default EmptyCart;
