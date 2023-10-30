e239c40e59cecf38760b23b3e64eece30fa78506
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-10-30 22:09:16 +0100

    Update '!=' to '!==' in {location.pathname} check

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./Header.module.css";
 import logo from "../assets/images/logo.png";
+
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectCart } from "../../redux/slices/cartSlice";
+
 const Header = () => {
   const { items, totalPrice } = useSelector(selectCart);
   const totalItems = items.reduce((sum, item) => sum + item.count, 0);
@@ -16,7 +18,7 @@ const Header = () => {
           <span className={styles.logoName}>Sushi paradise</span>
         </Link>
       </div>
-      {location.pathname != "/cart" && (
+      {location.pathname !== "/cart" && (
         <div className={styles.cartInfo}>
           <Link to="/cart" className={styles.price}>
             {totalPrice}$
