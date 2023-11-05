3e303df0958fff70b14e9db2fc689cb974aa4639
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-11-05 17:35:04 +0100

    Add addToCart button to items component

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import classes from "./Items.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { addItem } from "../../redux/slices/cartSlice";
-import { selectCartItemById } from "../../redux/slices/cartSlice";
 import { Link } from "react-router-dom";
+import { AddToCartButton } from "../CartBlock/AddToCartBtn";
 
 const sauceType = ["unagi sauce", "teriyaki sauce"];
 
 const Items = ({ id, image, title, price, sauce }) => {
   const [activeId, setActiveId] = React.useState(0);
-  const dispatch = useDispatch();
-
-  const cartItem = useSelector(selectCartItemById(id));
-  const addedCount = cartItem ? cartItem.count : 0;
-  const onClickAdd = () => {
-    const item = {
-      id,
-      image,
-      title,
-      price,
-      sauce: sauceType[activeId],
-    };
-    dispatch(addItem(item));
-  };
   return (
     <div className={classes.productContainer}>
       <div className={classes.imgContainer}>
@@ -45,12 +29,14 @@ const Items = ({ id, image, title, price, sauce }) => {
         </ul>
         <div className={classes.priceDetails}>
           <span className={classes.price}>${price}</span>
-          <button onClick={onClickAdd} className={classes.orderBtn}>
-            Add to cart{" "}
-            {addedCount > 0 && (
-              <span className={classes.itemAmount}>{addedCount}</span>
-            )}
-          </button>
+          <AddToCartButton
+            id={id}
+            image={image}
+            title={title}
+            price={price}
+            activeId={activeId}
+            sauceType={sauceType}
+          />
         </div>
       </div>
     </div>
