f4bb2eade8c327e206992cf07864c76ef2416c09
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-11-09 21:56:06 +0100

    Add container to form

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import classes from "./Checkout.module.css";
 
 export const Checkout = () => {
   const form = useRef();
@@ -25,49 +26,51 @@ export const Checkout = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <div className={classes.formGroup}>
-        <input
-          type="text"
-          required
-          className={classes.fieldInp}
-          name="user_name"
-        />
-        <label>Name and Surname</label>
-      </div>
-      <div className={classes.formGroup}>
-        <input
-          type="text"
-          required
-          className={classes.fieldInp}
-          name="user_email"
-        />
-        <label>Email</label>
-      </div>
-      <div className={classes.formGroup}>
-        <input
-          type="number"
-          required
-          className={classes.fieldInp}
-          name="user_phone"
-        />
-        <label>Phone</label>
-      </div>
-      <div className={classes.formGroup}>
-        <input
-          type="text"
-          required
-          className={classes.fieldInp}
-          name="user_address"
-        />
-        <label>Address(street, house)</label>
-      </div>
-      <div className={classes.formGroup}>
-        <textarea required name="message" className={classes.fieldInp} />
-        <label>Special wishes</label>
-      </div>
-      <input type="submit" value="Send" />
-    </form>
+    <div className={classes.checkoutContainer}>
+      <form ref={form} onSubmit={sendEmail}>
+        <div className={classes.formGroup}>
+          <input
+            type="text"
+            required
+            className={classes.fieldInp}
+            name="user_name"
+          />
+          <label>Name and Surname</label>
+        </div>
+        <div className={classes.formGroup}>
+          <input
+            type="text"
+            required
+            className={classes.fieldInp}
+            name="user_email"
+          />
+          <label>Email</label>
+        </div>
+        <div className={classes.formGroup}>
+          <input
+            type="number"
+            required
+            className={classes.fieldInp}
+            name="user_phone"
+          />
+          <label>Phone</label>
+        </div>
+        <div className={classes.formGroup}>
+          <input
+            type="text"
+            required
+            className={classes.fieldInp}
+            name="user_address"
+          />
+          <label>Address(street, house)</label>
+        </div>
+        <div className={classes.formGroup}>
+          <textarea required name="message" className={classes.fieldInp} />
+          <label>Special wishes</label>
+        </div>
+        <input type="submit" value="Send" />
+      </form>
+    </div>
   );
 };
 export default Checkout;
