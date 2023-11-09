f411890d31d81d7deab1b62e93ed1a66f727ef54
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-11-09 22:10:58 +0100

    Add error component

---
+++ src/pages/Error.jsx
@@ -0,0 +1,10 @@
+import "./Error.module.css";
+const Error = () => {
+  return (
+    <h4 className="error">
+      We're currently experiencing a high volume of requests. Please try again
+      in a moment.
+    </h4>
+  );
+};
+export default Error;
