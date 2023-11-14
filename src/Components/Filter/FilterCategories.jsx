228a747ac407216b1fd387d3a8d94f521314521b
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-11-14 20:59:10 +0100

    Display icons for sushi categories

--- src/Components/Filter/FilterCategories.jsx
+++ src/Components/Filter/FilterCategories.jsx
@@ -1,17 +1,33 @@
 import classes from "./FilterCategories.module.css";
+import all from "../assets/images/all.png";
+import premium from "../assets/images/premium.png";
+import rolls from "../assets/images/rolls.png";
+import royal from "../assets/images/royal.png";
+import sets from "../assets/images/sets.png";
 
 const FilterCategories = ({ categoryId, onClickCategory }) => {
-  const categories = ["All", "Premium", "Rolls", "Royal", "Sushi sets"];
+  const categories = [
+    { name: "All", image: all },
+    { name: "Premium", image: premium },
+    { name: "Rolls", image: rolls },
+    { name: "Royal", image: royal },
+    { name: "Sets", image: sets },
+  ];
   return (
     <>
       <ul className={classes.listWrapper}>
         {categories.map((value, i) => (
           <li
-            key={value}
+            key={i}
             onClick={() => onClickCategory(i)}
             className={categoryId === i ? classes.active : ""}
           >
-            {value}
+            <img
+              src={value.image}
+              alt={value.name}
+              className={classes.categoriesImgs}
+            />
+            {value.name}
           </li>
         ))}
       </ul>
