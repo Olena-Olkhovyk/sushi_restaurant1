2cb46c5cf5971e227c400a350ae5a9a84043157f
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-11-16 17:50:53 +0100

    Render ConfirmMessage component instead of alert

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,12 @@
 import { useRef } from "react";
+import React from "react";
 import emailjs from "@emailjs/browser";
 import classes from "./Checkout.module.css";
+import ConfirmMessage from "../Components/ConfirmMessage/ConfirmMessage";
 
 export const Checkout = () => {
   const form = useRef();
+  const [showConfirmation, setShowConfirmation] = React.useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -17,9 +20,7 @@ export const Checkout = () => {
       )
       .then(
         () => {
-          alert(
-            "Thank you for your order! We will deliver the sushi as quickly as possible."
-          );
+          setShowConfirmation(true);
         },
         (error) => {
           alert("There is an " + error);
@@ -29,50 +30,57 @@ export const Checkout = () => {
 
   return (
     <div className={classes.checkoutContainer}>
-      <h2>To confirm your order, please submit the form</h2>
-      <form ref={form} onSubmit={sendEmail}>
-        <div className={classes.formGroup}>
-          <input
-            type="text"
-            required
-            className={classes.fieldInp}
-            name="user_name"
-          />
-          <label>Name and Surname</label>
+      {showConfirmation ? (
+        <ConfirmMessage />
+      ) : (
+        <div>
+          <h2>To confirm your order, please submit the form</h2>
+
+          <form ref={form} onSubmit={sendEmail}>
+            <div className={classes.formGroup}>
+              <input
+                type="text"
+                required
+                className={classes.fieldInp}
+                name="user_name"
+              />
+              <label>Name and Surname</label>
+            </div>
+            <div className={classes.formGroup}>
+              <input
+                type="text"
+                required
+                className={classes.fieldInp}
+                name="user_email"
+              />
+              <label>Email</label>
+            </div>
+            <div className={classes.formGroup}>
+              <input
+                type="number"
+                required
+                className={classes.fieldInp}
+                name="user_phone"
+              />
+              <label>Phone</label>
+            </div>
+            <div className={classes.formGroup}>
+              <input
+                type="text"
+                required
+                className={classes.fieldInp}
+                name="user_address"
+              />
+              <label>Address(street, house)</label>
+            </div>
+            <div className={classes.formGroup}>
+              <textarea required name="message" className={classes.fieldInp} />
+              <label>Special wishes</label>
+            </div>
+            <input type="submit" value="Send" />
+          </form>
         </div>
-        <div className={classes.formGroup}>
-          <input
-            type="text"
-            required
-            className={classes.fieldInp}
-            name="user_email"
-          />
-          <label>Email</label>
-        </div>
-        <div className={classes.formGroup}>
-          <input
-            type="number"
-            required
-            className={classes.fieldInp}
-            name="user_phone"
-          />
-          <label>Phone</label>
-        </div>
-        <div className={classes.formGroup}>
-          <input
-            type="text"
-            required
-            className={classes.fieldInp}
-            name="user_address"
-          />
-          <label>Address(street, house)</label>
-        </div>
-        <div className={classes.formGroup}>
-          <textarea required name="message" className={classes.fieldInp} />
-          <label>Special wishes</label>
-        </div>
-        <input type="submit" value="Send" />
-      </form>
+      )}
     </div>
   );
 };
