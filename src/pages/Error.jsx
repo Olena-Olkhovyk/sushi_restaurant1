a00fd9fd16bf8164ff84d4bbc69b5a34e37b175b
Olena-Olkhovyk <alyonaeremen@gmail.com>
2023-12-02 11:10:39 +0100

    Style errorBlock component

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,12 @@
-import "./Error.module.css";
+import classes from "./Error.module.css";
 const Error = () => {
   return (
-    <h4 className="error">
-      We're currently experiencing a high volume of requests. Please try again
-      in a moment.
-    </h4>
+    <div className={classes.erorBlock}>
+      <h4 className={classes.error}>
+        We're currently experiencing a high volume of requests. Please try again
+        in a moment.
+      </h4>
+    </div>
   );
 };
 export default Error;
